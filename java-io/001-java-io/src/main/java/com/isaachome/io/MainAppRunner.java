9e09691a3abb073e81aa83887fc24f699ae3056d
theisaachome <65864160+theisaachome@users.noreply.github.com>
2023-04-30 18:32:22 +0630

    FileOutput Demo

---
+++ java-io/001-java-io/src/main/java/com/isaachome/io/MainAppRunner.java
@@ -0,0 +1,10 @@
+package com.isaachome.io;
+
+import java.io.FileOutputStream;
+
+public class MainAppRunner {
+    public static void main(String[] args) {
+        System.out.println("Hello from Java IO");
+        FileOutputStream fileOutputStream;
+    }
+}
